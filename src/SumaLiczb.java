464840451219cc1fe7198335d329575949ccd61b
Costa Robe <56robik@gmail.com>
2026-03-05 23:14:19 +0100

    Wydzielenie metod Kolejny problem w przedstawionym programie to ilość kodu zapisanego w metodzie main. Kod powinien odzwierciedlać kolejne kroki algorytmu. W naszym przypadku ciężko jest wyróżnić poszczególne etapy działania programu, a powinniśmy tutaj jasno widzieć:
    wczytanie liczb od użytkownika,
    wydzielenie liczb dodatnich,
    wydzielenie liczb ujemnych
    obliczenie sumy liczb,
    wyświetlenie wyników.
    W celu wydzielenia fragmentu kodu do osobnej metody zaznacz wybrany fragment kodu, kliknij prawym przyciskiem myszy, a następnie wybierz opcję Refactor > Extract Method. Możesz też skorzystać ze skrótu klawiaturowego Alt + Enter lub Option + Enter.

--- src/SumaLiczb.java
+++ src/SumaLiczb.java
@@ -4,6 +4,54 @@ import java.util.Scanner;
 
 class NumbersOperations {
     public static void main(String[] args) {
+        List<Integer> userNumbers = getNumbersFromUser();
+        List<Integer> positiveNumbers = getPositiveNumbers(userNumbers);
+        List<Integer> negativeNumbers = getNegativeNumbers(userNumbers);
+        int positiveNumbersSum = positiveNumbersSum(positiveNumbers);
+        int negativeNumbersSum = negativeNumbersSum(negativeNumbers);
+        printResults(positiveNumbersSum, negativeNumbersSum);
+    }
+
+    private static void printResults(int positiveNumbersSum, int negativeNumbersSum) {
+        System.out.println("Suma wprowadzonych liczb dodatnich: " + positiveNumbersSum);
+        System.out.println("Suma wprowadzonych liczb ujemnych: " + negativeNumbersSum);
+    }
+
+    private static int negativeNumbersSum(List<Integer> negativeNumbers) {
+        int negativeNumbersSum = 0;
+        for (int i = 0; i < negativeNumbers.size(); i++) {
+            negativeNumbersSum += negativeNumbers.get(i);
+        }
+        return negativeNumbersSum;
+    }
+
+    private static int positiveNumbersSum(List<Integer> positiveNumbers) {
+        int positiveNumbersSum = 0;
+        for (int i = 0; i < positiveNumbers.size(); i++) {
+            positiveNumbersSum += positiveNumbers.get(i);
+        }
+        return positiveNumbersSum;
+    }
+
+    private static List<Integer> getNegativeNumbers(List<Integer> userNumbers) {
+        List<Integer> negativeNumbers = new ArrayList<>();
+        for (int i = 0; i < userNumbers.size(); i++) {
+            if (userNumbers.get(i) < 0)
+                negativeNumbers.add(userNumbers.get(i));
+        }
+        return negativeNumbers;
+    }
+
+    private static List<Integer> getPositiveNumbers(List<Integer> userNumbers) {
+        List<Integer> positiveNumbers = new ArrayList<>();
+        for (int i = 0; i < userNumbers.size(); i++) {
+            if (userNumbers.get(i) >= 0)
+                positiveNumbers.add(userNumbers.get(i));
+        }
+        return positiveNumbers;
+    }
+
+    private static List<Integer> getNumbersFromUser() {
         Scanner scanner = new Scanner(System.in);
         List<Integer> userNumbers = new ArrayList<>();
         System.out.println("Ile liczb wczytać?");
@@ -12,25 +60,6 @@ class NumbersOperations {
             System.out.println("Podaj kolejną liczbę:");
             userNumbers.add(scanner.nextInt());
         }
-        List<Integer> positiveNumbers = new ArrayList<>();
-        for (int i = 0; i < userNumbers.size(); i++) {
-            if (userNumbers.get(i) >= 0)
-                positiveNumbers.add(userNumbers.get(i));
-        }
-        List<Integer> negativeNumbers = new ArrayList<>();
-        for (int i = 0; i < userNumbers.size(); i++) {
-            if (userNumbers.get(i) < 0)
-                negativeNumbers.add(userNumbers.get(i));
-        }
-        int positiveNumbersSum = 0;
-        for (int i = 0; i < positiveNumbers.size(); i++) {
-            positiveNumbersSum += positiveNumbers.get(i);
-        }
-        int negativeNumbersSum = 0;
-        for (int i = 0; i < negativeNumbers.size(); i++) {
-            negativeNumbersSum += negativeNumbers.get(i);
-        }
-        System.out.println("Suma wprowadzonych liczb dodatnich: " + positiveNumbersSum);
-        System.out.println("Suma wprowadzonych liczb ujemnych: " + negativeNumbersSum);
+        return userNumbers;
     }
 }
